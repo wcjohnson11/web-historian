16e687bc67da7f0f82be729d8577f1cb93d515a6
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-25 15:39:55 -0700

    Passed first test

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -10,9 +10,13 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
-exports.serveAssets = function(res, asset) {
+exports.serveAssets = function(res, asset, statusCode) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  statusCode = statusCode || 200;
+  res.writeHead(statusCode, headers);
+  res.end(asset);
 };
+
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,24 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var httpHelpers = require('./http-helpers.js');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  //serve index.html
+  if (req.url === '/' || req.url[1] === '?') {
+    var html =  fs.readFileSync(archive.paths.siteAssets + '/index.html');
+    httpHelpers.serveAssets(res, html, 200);
+  }
+  //on get request
+    //loop through sites file to srch for requested site
+      //if exists, serve file
+      //else if requested site in sites.txt
+        //serve loading page
+      //else write to sites.txt
+      //serve loading page
+
+  //res.end(archive.paths.list);
 };
