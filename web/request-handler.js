b3ad8557d7a9add56b4bd88723da3cd145513de8
Hack Reactor Students <williamcouperjohnson@gmail.com>
2014-06-25 17:23:55 -0700

    Serving up static HTML and CSS, now to implement routing

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,7 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
 var httpHelpers = require('./http-helpers.js');
+var http = require('http');
 
 // require more modules/folders here!
 
@@ -11,8 +12,25 @@ exports.handleRequest = function (req, res) {
   if (req.url === '/' || req.url[1] === '?') {
     var html =  fs.readFileSync(archive.paths.siteAssets + '/index.html');
     httpHelpers.serveAssets(res, html, 200);
+  } else if(req.url === '/styles.css'){
+     var css = fs.readFileSync(archive.paths.siteAssets + '/styles.css');
+     httpHelpers.serveAssets(res, css, 200);
+  }else if (req.method === 'POST'){
+    var html =  fs.readFileSync(archive.paths.archivedSites + req.url);
+    httpHelpers.serveAssets(res, html, 200);
+    //get the data
+    //if the data is in sites
+    //  serve file
+    // call get
+    //if the data is not in sites.txt
+    //  write to sites.txt
+    //
+    //
+  } else if (req.method === 'GET'){
+    httpHelpers.serveAssets(res, true, 200);
+  //serving the file
   }
-  //on get request
+
     //loop through sites file to srch for requested site
       //if exists, serve file
       //else if requested site in sites.txt
@@ -20,5 +38,5 @@ exports.handleRequest = function (req, res) {
       //else write to sites.txt
       //serve loading page
 
-  //res.end(archive.paths.list);
+ res.end(archive.paths.list);
 };
